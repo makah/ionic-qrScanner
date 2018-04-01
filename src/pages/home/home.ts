08eeb9f28a69018fcac1d639d5887de4f0806b55
Makah <arieiramau@gmail.com>
2018-04-01 15:45:05 +0000

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  results: String;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private barcode: BarcodeScanner) {
 
+  }
+  
+  async scanBarcode() {
+    
+    let options: BarcodeScannerOptions = {
+      prompt: 'Hora de scanear!'
+    }
+    
+    const results = await this.barcode.scan(options);
+    console.log(results);
   }
 
 }
